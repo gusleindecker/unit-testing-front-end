010b9c4b1443e33adf8d48fa3e486192707d08db
Gustavo Leindecker <723481+gusleindecker@users.noreply.github.com>
2023-08-16 12:25:04 -0300

    Small adjustment in the UserList unit test file

--- src/components/__tests__/UserList.test.ts
+++ src/components/__tests__/UserList.test.ts
@@ -1,14 +1,14 @@
 import { describe, it, vi, type Mock, expect } from "vitest";
-import UserList from "../UserList.vue";
+import UserList from "@/components/UserList.vue";
 import { render } from "@testing-library/vue";
 import { mockUsers } from "@/mocks";
 import { useRouter } from "vue-router";
 
 vi.mock("vue-router");
+const mockedUseRouter = vi.mocked(useRouter) as Mock;
 
 describe("components/UserList.vue", () => {
   it("Should display the list of users.", () => {
-    const mockedUseRouter = vi.mocked(useRouter) as Mock;
     mockedUseRouter.mockReturnValueOnce({ push: vi.fn() });
     const { getAllByTestId } = render(UserList, {
       props: { users: mockUsers },
